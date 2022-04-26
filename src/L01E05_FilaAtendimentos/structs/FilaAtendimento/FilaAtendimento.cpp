37819015f754ef91e55e8e1ce045cf256ff5a00e
Rubia Souza <rubensvictor.m.s@gmail.com>
2022-04-26 18:45:25 -0300

    Revert "refactor(VPL5): Remove ListaDuplamenteEncadeada do header"
    This reverts commit 7987300aa9eee9e88549bc91e061ed695a6ef74d.

--- src/L01E05_FilaAtendimentos/structs/FilaAtendimento/FilaAtendimento.cpp
+++ src/L01E05_FilaAtendimentos/structs/FilaAtendimento/FilaAtendimento.cpp
@@ -4,208 +4,195 @@
 #include "./FilaAtendimento.hpp"
 #include "../Cliente/Cliente.hpp"
 
-struct Celula {
-    Celula *proxima;
-    Celula *anterior;
-    Cliente valor;
+Celula::Celula() {
+    set_proxima(nullptr);
+    set_anterior(nullptr);
+    set_valor(*(new Cliente()));
+}
 
-    Celula() {
-        set_proxima(nullptr);
-        set_anterior(nullptr);
-        set_valor(*(new Cliente()));
-    }
+Celula::Celula(Cliente valor) {
+    set_proxima(nullptr);
+    set_anterior(nullptr);
+    set_valor(valor);
+}
 
-    Celula(Cliente valor) {
-        set_proxima(nullptr);
-        set_anterior(nullptr);
-        set_valor(valor);
-    }
+Celula::Celula(Celula* proxima, Celula* anterior) {
+    set_proxima(proxima);
+    set_anterior(anterior);
+    set_valor(*(new Cliente()));
+}
 
-    Celula(Celula *proxima, Celula *anterior) {
-        set_proxima(proxima);
-        set_anterior(anterior);
-        set_valor(*(new Cliente()));
-    }
+Celula::Celula(Celula* proxima, Celula* anterior, Cliente valor) {
+    set_proxima(proxima);
+    set_anterior(anterior);
+    set_valor(valor);
+}
 
-    Celula(Celula *proxima, Celula *anterior, Cliente valor) {
-        set_proxima(proxima);
-        set_anterior(anterior);
-        set_valor(valor);
-    }
+void Celula::set_proxima(Celula* proxima) {
+    this->proxima = proxima;
+    return;
+}
 
-    void set_proxima(Celula *proxima) {
-        this->proxima = proxima;
-        return;
-    }
+Celula* Celula::get_proxima() const {
+    return proxima;
+}
 
-    Celula* get_proxima() const {
-        return proxima;
-    }
+void Celula::set_anterior(Celula* anterior) {
+    this->anterior = anterior;
+    return;
+}
 
-    void set_anterior(Celula *anterior) {
-        this->anterior = anterior;
-        return;
-    }
+Celula* Celula::get_anterior() const {
+    return anterior;
+}
 
-    Celula* get_anterior() const {
-        return anterior;
-    }
+void Celula::set_valor(Cliente valor) {
+    this->valor = valor;
+    return;
+}
 
-    void set_valor(Cliente valor) {
-        this->valor = valor;
-        return;
-    }
+Cliente Celula::get_valor() const {
+    return valor;
+}
 
-    Cliente get_valor() const {
-        return valor;
-    }
-};
+ListaDuplamenteEncadeada::ListaDuplamenteEncadeada() {
+    inicio = new Celula();
+    fim = new Celula();
 
-struct ListaDuplamenteEncadeada {
-    Celula* inicio;
-    Celula* fim;
+    inicio->set_proxima(fim);
+    fim->set_anterior(inicio);
+    tamanho = 0;
+}
 
-    unsigned int tamanho;
+bool ListaDuplamenteEncadeada::esta_vazia() const {
+    return (inicio->proxima == fim && fim->anterior == inicio);
+}
 
-    ListaDuplamenteEncadeada() {
-        inicio = new Celula();
-        fim = new Celula();
+void ListaDuplamenteEncadeada::adicionar_ao_fim(const Cliente valor) {
+    Celula* novaCelula = new Celula(valor);
 
-        inicio->set_proxima(fim);
-        fim->set_anterior(inicio);
-        tamanho = 0;
-    }
+    Celula* penultimaCelula = fim->get_anterior();
+    penultimaCelula->set_proxima(novaCelula);
 
-    bool esta_vazia() const {
-        return (inicio->proxima == fim && fim->anterior == inicio);
-    }
+    novaCelula->set_anterior(penultimaCelula);
+    novaCelula->set_proxima(fim);
 
-    void adicionar_ao_fim(const Cliente valor) {
-        Celula *novaCelula = new Celula(valor);
+    fim->set_anterior(novaCelula);
+    tamanho++;
 
-        Celula *penultimaCelula = fim->get_anterior();
-        penultimaCelula->set_proxima(novaCelula);
+    return;
+}
 
-        novaCelula->set_anterior(penultimaCelula);
-        novaCelula->set_proxima(fim);
+void ListaDuplamenteEncadeada::adicionar_ao_comeco(const Cliente valor) {
+    Celula* novaCelula = new Celula(valor);
 
-        fim->set_anterior(novaCelula);
-        tamanho++;
+    Celula* segundaCelula = inicio->get_proxima();
+    segundaCelula->set_anterior(novaCelula);
 
-        return;
-    }
+    novaCelula->set_proxima(segundaCelula);
+    novaCelula->set_anterior(inicio);
 
-    void adicionar_ao_comeco(const Cliente valor) {
-        Celula *novaCelula = new Celula(valor);
+    inicio->set_proxima(novaCelula);
+    tamanho++;
 
-        Celula *segundaCelula = inicio->get_proxima();
-        segundaCelula->set_anterior(novaCelula);
+    return;
+}
 
-        novaCelula->set_proxima(segundaCelula);
-        novaCelula->set_anterior(inicio);
-
-        inicio->set_proxima(novaCelula);
-        tamanho++;
-
-        return;
-    }
-
-    void adicionar_antes(const Cliente valor, const int senhaReferencia) {
-        Celula *posterior = nullptr;
-        for (posterior = inicio->get_proxima(); posterior != fim; posterior = posterior->get_proxima()) {
-            if (posterior->get_valor().get_senha() == senhaReferencia) {
-                break;
-            }
+void ListaDuplamenteEncadeada::adicionar_antes(const Cliente valor, const int senhaReferencia) {
+    Celula* posterior = nullptr;
+    for(posterior = inicio->get_proxima(); posterior != fim; posterior = posterior->get_proxima()) {
+        if(posterior->get_valor().get_senha() == senhaReferencia) {
+            break;
         }
+    }
 
-        Celula *anterior = posterior->get_anterior();
-        Celula *novaCelula = new Celula(valor);
+    Celula* anterior = posterior->get_anterior();
+    Celula* novaCelula = new Celula(valor);
 
-        anterior->set_proxima(novaCelula);
-        posterior->set_anterior(novaCelula);
+    anterior->set_proxima(novaCelula);
+    posterior->set_anterior(novaCelula);
 
-        novaCelula->set_anterior(anterior);
-        novaCelula->set_proxima(posterior);
+    novaCelula->set_anterior(anterior);
+    novaCelula->set_proxima(posterior);
 
-        tamanho++;
+    tamanho++;
+    return;
+}
+
+void ListaDuplamenteEncadeada::adicionar_depois(const Cliente valor, const int senhaReferencia) {
+    Celula* anterior = nullptr;
+    for(anterior = inicio->get_proxima(); anterior != fim; anterior = anterior->get_proxima()) {
+        if(anterior->get_valor().get_senha() == senhaReferencia) {
+            break;
+        }
+    }
+
+    Celula* posterior = anterior->get_proxima();
+    Celula* novaCelula = new Celula(valor);
+
+    anterior->set_proxima(novaCelula);
+    posterior->set_anterior(novaCelula);
+
+    novaCelula->set_anterior(anterior);
+    novaCelula->set_proxima(posterior);
+
+    tamanho++;
+    return;
+}
+
+void ListaDuplamenteEncadeada::remover(const Cliente valor) {
+    Celula* alvo = nullptr;
+    for(alvo = inicio->get_proxima(); alvo != fim; alvo = alvo->get_proxima()) {
+        if(alvo->get_valor().get_senha() == valor.get_senha()) {
+            break;
+        }
+    }
+
+    if(alvo == fim) {
         return;
     }
 
-    void adicionar_depois(const Cliente valor, const int senhaReferencia) {
-        Celula *anterior = nullptr;
-        for (anterior = inicio->get_proxima(); anterior != fim; anterior = anterior->get_proxima()) {
-            if (anterior->get_valor().get_senha() == senhaReferencia) {
-                break;
-            }
-        }
+    Celula* anterior = alvo->get_anterior();
+    Celula* posterior = alvo->get_proxima();
 
-        Celula *posterior = anterior->get_proxima();
-        Celula *novaCelula = new Celula(valor);
+    anterior->set_proxima(posterior);
+    posterior->set_anterior(anterior);
 
-        anterior->set_proxima(novaCelula);
-        posterior->set_anterior(novaCelula);
+    delete alvo;
+    return;
+}
 
-        novaCelula->set_anterior(anterior);
-        novaCelula->set_proxima(posterior);
-
-        tamanho++;
-        return;
+void ListaDuplamenteEncadeada::limpar() {
+    Celula* proximaReferencia = nullptr;
+    
+    for(Celula* iterador = inicio->get_proxima(); iterador != fim; iterador = proximaReferencia) {
+        proximaReferencia = iterador->get_proxima();
+        delete iterador;
     }
 
-    void remover(const Cliente valor) {
-        Celula *alvo = nullptr;
-        for (alvo = inicio->get_proxima(); alvo != fim; alvo = alvo->get_proxima()) {
-            if (alvo->get_valor().get_senha() == valor.get_senha()) {
-                break;
-            }
-        }
+    inicio->set_proxima(fim);
+    fim->set_anterior(inicio);
+    tamanho = 0;
 
-        if (alvo == fim) {
-            return;
-        }
+    return;
+}
 
-        Celula *anterior = alvo->get_anterior();
-        Celula *posterior = alvo->get_proxima();
-
-        anterior->set_proxima(posterior);
-        posterior->set_anterior(anterior);
-
-        delete alvo;
-        return;
+Cliente ListaDuplamenteEncadeada::get_elemento_em(const unsigned int posicao) const {
+    if(posicao >= get_tamanho()) {
+        return *(new Cliente());
     }
 
-    void limpar() {
-        Celula *proximaReferencia = nullptr;
-
-        for (Celula *iterador = inicio->get_proxima(); iterador != fim; iterador = proximaReferencia) {
-            proximaReferencia = iterador->get_proxima();
-            delete iterador;
-        }
-
-        inicio->set_proxima(fim);
-        fim->set_anterior(inicio);
-        tamanho = 0;
-
-        return;
+    Celula* alvo = inicio->get_proxima();
+    for(unsigned int i = 0; i < posicao; i++) {
+        alvo = alvo->get_proxima();
     }
 
-    Cliente get_elemento_em(const unsigned int posicao) const {
-        if (posicao >= get_tamanho()) {
-            return *(new Cliente());
-        }
+    return alvo->get_valor();
+}
 
-        Celula *alvo = inicio->get_proxima();
-        for (unsigned int i = 0; i < posicao; i++) {
-            alvo = alvo->get_proxima();
-        }
-
-        return alvo->get_valor();
-    }
-
-    unsigned int get_tamanho() const {
-        return tamanho;
-    }    
-};
+unsigned int ListaDuplamenteEncadeada::get_tamanho() const {
+    return tamanho;
+}
 
 /*void FilaAtendimento::adicionar_cliente(const std::string nome, const unsigned int idade) {
 
