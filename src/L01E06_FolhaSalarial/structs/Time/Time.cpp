718506bc024fca7b2411617aacdc1197be39c813
Rubia Souza <rubensvictor.m.s@gmail.com>
2022-04-28 11:10:51 -0300

    feat(VPL5): Implemente Time

---
+++ src/L01E06_FolhaSalarial/structs/Time/Time.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include <algorithm>
+
+#include "./Time.hpp"
+#include "../Jogador/Jogador.hpp"
+
+Time::Time() {
+    set_nome("");
+    jogadores = *(new std::list<Jogador>());
+}
+
+Time::Time(const std::string nome) {
+    set_nome(nome);
+    jogadores = *(new std::list<Jogador>());
+}
+
+void Time::adicionar_jogador(const std::string nome, const std::string posicao, const unsigned int salario) {
+    Jogador* jogador = new Jogador(nome, posicao, salario);
+    jogadores.push_back(*jogador);
+
+    return;
+}
+
+void Time::remover_jogador(const std::string& nome, const std::string& posicao) {
+    for(std::list<Jogador>::iterator iterador = jogadores.begin(); iterador != jogadores.end(); iterador++) {
+        if(iterador->get_nome() == nome && iterador->get_posicao() == posicao) {
+            jogadores.erase(iterador);
+        }
+    }
+
+    return;
+}
+
+Jogador Time::get_jogador(const std::string& nome) const {
+    for(Jogador jogador : jogadores) {
+        if(jogador.get_nome() == nome) {
+            return jogador;
+        }
+    }
+
+    return *(new Jogador());
+}
+
+void Time::imprimir_lista_jogadores() const {
+    std::cout << get_nome() << "\n";
+    jogadores.sort([](const Jogador& jogador1, const Jogador& jogador2) {
+        return jogador2.get_salario() < jogador1.get_salario();
+    });
+
+    for(Jogador jogador : jogadores) {
+        jogador.imprimir_dados();
+    }
+
+    return;
+}
+
+void Time::imprimir_folha_salarial_consolidada() const {
+    unsigned int salario_total = 0;
+
+    for(Jogador jogador : jogadores) {
+        salario_total += jogador.get_salario();
+    }
+
+    std::cout << get_nome() << "\t" << salario_total << "\n";
+    return;
+}
+
+void Time::set_nome(const std::string nome) {
+    this->nome = nome;
+    return;
+}
+
+std::string Time::get_nome() const {
+    return nome;
+}
+
+std::list<Jogador> Time::get_jogadores() const {
+    return jogadores;
+}
---
+++ src/L01E06_FolhaSalarial/structs/Time/Time.hpp
@@ -0,0 +1,29 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <list>
+#include <string>
+
+#include "../Jogador/Jogador.hpp"
+
+struct Time {
+    std::string nome;
+    std::list<Jogador> jogadores;
+
+    Time();
+    Time(const std::string nome);
+
+    void adicionar_jogador(const std::string nome, const std::string posicao, const unsigned int salario);
+    void remover_jogador(const std::string& nome, const std::string& posicao);
+    Jogador get_jogador(const std::string& nome) const;
+
+    void imprimir_lista_jogadores() const;
+    void imprimir_folha_salarial_consolidada() const;
+
+    void set_nome(const std::string nome);
+    std::string get_nome() const;
+
+    std::list<Jogador> get_jogadores() const;
+};
+
+#endif
