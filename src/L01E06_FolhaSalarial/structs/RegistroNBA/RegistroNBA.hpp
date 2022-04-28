d702c9429165385c301a73292ae6e484d50c2013
Rubia Souza <rubensvictor.m.s@gmail.com>
2022-04-28 11:11:29 -0300

    feat(VPL6): Adiciona assinatura RegistroNBA

---
+++ src/L01E06_FolhaSalarial/structs/RegistroNBA/RegistroNBA.hpp
@@ -0,0 +1,24 @@
+#ifndef REGISTRO_NBA_H
+#define REGISTRO_NBA_H
+
+#include <list>
+#include <string>
+
+#include "../Time/Time.hpp"
+
+struct RegistroNBA {
+    std::list<Time> times;
+
+    RegistroNBA();
+
+    void adicionar_time(const std::string nome);
+    void adicionar_jogador(const std::string nome_time, const std::string nome_jogador, const std::string posicao, const unsigned int salario);
+
+    void imprimir_lista_jogadores_time(const std::string nome_time) const;
+    void imprimir_folha_consolidada_time(const std::string nome_time) const;
+    void imprimir_folha_salarial_geral() const;
+
+    std::list<Time> get_times() const;
+};
+
+#endif
