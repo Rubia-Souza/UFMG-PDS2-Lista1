541b33913717874d1be7c4731665fcc8b9ca9ed0
Rubia Souza <rubensvictor.m.s@gmail.com>
2022-04-28 09:12:06 -0300

    feat(VPL6): Adiciona Jogador

---
+++ src/L01E06_FolhaSalarial/structs/Jogador/Jogador.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+#include "./Jogador.hpp"
+
+Jogador::Jogador() {
+    set_nome("");
+    set_posicao("");
+    set_salario(0);
+}
+
+Jogador::Jogador(const std::string nome) {
+    set_nome(nome);
+    set_posicao("");
+    set_salario(0);
+}
+
+Jogador::Jogador(const std::string nome, const std::string posicao, const unsigned int salario) {
+    set_nome(nome);
+    set_posicao(posicao);
+    set_salario(salario);
+}
+
+void Jogador::imprimir_dados() const {
+    std::cout << get_nome() << "\t" << get_posicao() << "\t" << get_salario() << "\n";
+    return;
+}
+
+void Jogador::set_nome(const std::string nome) {
+    this->nome = nome;
+    return;
+}
+
+std::string Jogador::get_nome() const {
+    return nome;
+}
+
+void Jogador::set_posicao(const std::string posicao) {
+    this->posicao = posicao;
+    return;
+}
+
+std::string Jogador::get_posicao() const {
+    return posicao;
+}
+
+void Jogador::set_salario(const unsigned int salario){
+    this->salario = salario;
+    return;
+}
+
+unsigned int Jogador::get_salario() const {
+    return salario;
+}
---
+++ src/L01E06_FolhaSalarial/structs/Jogador/Jogador.hpp
@@ -0,0 +1,27 @@
+#ifndef JOGADOR_H
+#define JOGADOR_H
+
+#include <string>
+
+struct Jogador {
+    std::string nome;
+    std::string posicao;
+    unsigned int salario;
+
+    Jogador();
+    Jogador(const std::string nome);
+    Jogador(const std::string nome, const std::string posicao, const unsigned int salario);
+
+    void imprimir_dados() const;
+
+    void set_nome(const std::string nome);
+    std::string get_nome() const;
+
+    void set_posicao(const std::string posicao);
+    std::string get_posicao() const;
+
+    void set_salario(const unsigned int salario);
+    unsigned int get_salario() const;
+};
+
+#endif
