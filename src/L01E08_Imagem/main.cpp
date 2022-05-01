efc19641bf514deb8b4d3ff3727b63b6b44d1447
Rubia Souza <rubensvictor.m.s@gmail.com>
2022-05-01 14:12:04 -0300

    feat(VPL8): Cria estrutura base do main

---
+++ src/L01E08_Imagem/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#define INICIALIZAR_IMAGEN 'i'
+#define PREENCHER_IMAGEM 'p'
+#define EXIBIR_IMAGEM 'i'
+#define TRANSFORMAR_TONS_CINZA 'g'
+#define BINARIZAR 't'
+#define AVALICAO_BASICA 'b'
+
+int main() {
+
+    return 0;
+}
