97b19732120cc2ca714af8a184d9d13fd1ed0dee
Rubia Souza <rubensvictor.m.s@gmail.com>
2022-05-02 19:42:19 -0300

    feat(VPL8): Implmenta print()

--- src/L01E08_Imagem/structs/Pixel/Pixel.cpp
+++ src/L01E08_Imagem/structs/Pixel/Pixel.cpp
@@ -1,6 +1,10 @@
+#include <iostream>
 #include <vector>
+#include <string>
 
 #include "./Pixel.hpp"
+
+std::vector<std::string> formatar_cores(const unsigned short int red, const unsigned short int green, const unsigned short int blue);
 
 Pixel::Pixel() {
     set_rgb(0, 0, 0);
@@ -10,7 +14,33 @@ Pixel::Pixel(const unsigned short int red, const unsigned short int green, const
     set_rgb(red, blue, green);
 }
 
-void Pixel::print() const {}
+void Pixel::print() const {
+    std::vector<std::string> cores = formatar_cores(get_red(), get_blue(), get_green());
+
+    std::cout << cores[0] << cores[1] << cores[2];
+
+    return;
+}
+
+std::vector<std::string> formatar_cores(const unsigned short int red, const unsigned short int green, const unsigned short int blue) {
+    std::vector<std::string> cores;
+    cores.push_back(std::to_string(red));
+    cores.push_back(std::to_string(green));
+    cores.push_back(std::to_string(blue));
+
+    std::string cores_formatadas = "";
+
+    for(unsigned int i = 0; i < cores.size(); i++) {
+        if(cores[i].size() == 1) {
+            cores[i].insert(0, "00");
+        }
+        else if(cores[i].size() == 2) {
+            cores[i].insert(0, "0");
+        }
+    }
+
+    return cores;
+}
 
 void Pixel::set_rgb(const unsigned short int red, const unsigned short int green, const unsigned short int blue) {
     set_red(red);
